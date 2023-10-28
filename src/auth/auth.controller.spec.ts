c18503752ff5fb07ddd95d4591efa51184412a92
Maganez Filho <maganezfilho@gmail.com>
2023-10-28 14:31:26 -0300

    feat(auth module): unit and integration testing added

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,52 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { WaitersService } from '../waiters/waiters.service';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        {
+          provide: WaitersService,
+          useValue: {
+            findOneByUsername: jest.fn(),
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+  });
+
+  it('should return a token on login', async () => {
+    const mockUser: LoginRequest = { user: { id: 1, username: 'testuser' } };
+
+    jest.spyOn(authService, 'login').mockImplementation(() => {
+      return Promise.resolve({
+        message: 'Login made successfully',
+        data: { token: 'generated-token' },
+      });
+    });
+    const result = await authController.login(mockUser);
+    expect(result).toHaveProperty('data');
+    expect(result.data).toHaveProperty('token', 'generated-token');
   });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,73 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { WaiterDto } from '../waiters/dto/waiter.dto';
+import { WaitersService } from '../waiters/waiters.service';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let waitersService: WaitersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: WaitersService,
+          useValue: {
+            findOneByUsername: jest.fn(),
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    waitersService = module.get<WaitersService>(WaitersService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  it('should validate user credentials', async () => {
+    const mockWaiter: WaiterDto = {
+      id: 'user-id',
+      username: 'testuser',
+      password: 'password123',
+    };
+    jest.spyOn(waitersService, 'findOneByUsername').mockImplementation(() => {
+      return Promise.resolve(mockWaiter);
+    });
+
+    jest
+      .spyOn(authService, 'validateUser')
+      .mockImplementation(async (username, password) => {
+        if (username === 'testuser' && password === 'password123') {
+          return mockWaiter;
+        } else {
+          return null;
+        }
+      });
+
+    const result = await authService.validateUser('testuser', 'password123');
+    expect(result).toEqual(mockWaiter);
+  });
+
+  it('should return null for invalid user credentials', async () => {
+    jest.spyOn(waitersService, 'findOneByUsername').mockImplementation(() => {
+      return Promise.resolve(null);
+    });
+
+    const result = await authService.validateUser(
+      'invaliduser',
+      'invalidpassword'
+    );
+    expect(result).toBeNull();
   });
 });
