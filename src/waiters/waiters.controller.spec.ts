efb49a5752dfd9023d988a945fdc5606e06f9c04
Maganez Filho <maganezfilho@gmail.com>
2023-10-28 20:49:00 -0300

    feat(waiters module): unit and integration testing added

--- src/waiters/waiters.controller.spec.ts
+++ src/waiters/waiters.controller.spec.ts
@@ -1,20 +1,156 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ResponseDto } from '../@types';
+import { CreateWaiterDto } from './dto/create-waiter.dto';
+import { WaiterEntity } from './entities/waiter.entity';
 import { WaitersController } from './waiters.controller';
 import { WaitersService } from './waiters.service';
 
 describe('WaitersController', () => {
-  let controller: WaitersController;
+  let waitersController: WaitersController;
+  let waitersService: WaitersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WaitersController],
-      providers: [WaitersService],
+      providers: [
+        WaitersService,
+        {
+          provide: getRepositoryToken(WaiterEntity),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<WaitersController>(WaitersController);
+    waitersController = module.get<WaitersController>(WaitersController);
+    waitersService = module.get<WaitersService>(WaitersService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(waitersController).toBeDefined();
+  });
+
+  it('should create a new waiter', async () => {
+    const newWaiter: CreateWaiterDto = {
+      username: 'new-waiter-1',
+      password: '123',
+    };
+
+    const expectedNewWaiterResponse: ResponseDto<WaiterEntity> = {
+      message: 'success on create new waiter',
+      data: {
+        id: 'waiter-id',
+        username: 'new-waiter',
+        password: '123',
+        hashPassword: undefined,
+      },
+    };
+
+    jest
+      .spyOn(waitersService, 'create')
+      .mockResolvedValue(expectedNewWaiterResponse);
+
+    const result = await waitersController.create(newWaiter);
+    expect(result).toEqual(expectedNewWaiterResponse);
+  });
+
+  it('should find all waiters', async () => {
+    const expectedWaitersResponse: ResponseDto<WaiterEntity[]> = {
+      message: 'Success on finding all Waiters',
+      data: [
+        {
+          id: 'waiter-id-1',
+          username: 'new-waiter-1',
+          password: '123',
+          hashPassword: undefined,
+        },
+        {
+          id: 'waiter-id-2',
+          username: 'new-waiter-2',
+          password: '123',
+          hashPassword: undefined,
+        },
+      ],
+    };
+
+    jest
+      .spyOn(waitersService, 'findAll')
+      .mockResolvedValue(expectedWaitersResponse);
+
+    const result = await waitersController.findAll();
+    expect(result).toEqual(expectedWaitersResponse);
+  });
+
+  it('should find a waiter by ID', async () => {
+    const waiterId = 'waiter-id-1';
+    const expectedWaiterResponse: ResponseDto<WaiterEntity> = {
+      message: 'Success on finding waiter by ID',
+      data: {
+        id: 'waiter-id-1',
+        username: 'new-waiter-1',
+        password: '123',
+        hashPassword: undefined,
+      },
+    };
+
+    jest
+      .spyOn(waitersService, 'findOne')
+      .mockResolvedValue(expectedWaiterResponse);
+
+    const result = await waitersController.findOne(waiterId);
+    expect(result).toEqual(expectedWaiterResponse);
+  });
+
+  it('should update a waiter', async () => {
+    const waiterId = 'waiter-id-1';
+    const updatedWaiter: WaiterEntity = {
+      id: 'updated-waiter-id-1',
+      username: 'updated-new-waiter-1',
+      password: 'updated-123',
+      hashPassword: undefined,
+    };
+
+    const expectedUpdatedWaiterResponse: ResponseDto<Partial<WaiterEntity>> = {
+      message: 'Success on updating waiter',
+      data: updatedWaiter,
+    };
+
+    jest
+      .spyOn(waitersService, 'update')
+      .mockResolvedValue(expectedUpdatedWaiterResponse);
+
+    const result = await waitersController.update(
+      { id: waiterId },
+      updatedWaiter
+    );
+    expect(result).toEqual(expectedUpdatedWaiterResponse);
+  });
+
+  it('should remove a waiter by ID', async () => {
+    const waiterId = 'waiter-id-1';
+    const expectedRemoveWaiterResponse: ResponseDto<WaiterEntity[]> = {
+      message: 'Success on removing waiter by ID',
+      data: [
+        {
+          id: 'waiter-id-2',
+          username: 'new-waiter-2',
+          password: '123',
+          hashPassword: undefined,
+        },
+      ],
+    };
+
+    jest
+      .spyOn(waitersService, 'remove')
+      .mockResolvedValue(expectedRemoveWaiterResponse);
+
+    const result = await waitersService.remove(waiterId);
+    expect(result).toEqual(expectedRemoveWaiterResponse);
   });
 });
